2e2ad4ab984181162e533f025b8ac621b2426356
Stas <stasfuryus@gmail.com>
2024-07-06 11:43:50 +0300

    urls config + fix, add setuptools

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "django_filters",
-    'rest_framework_simplejwt',
-    'drf_yasg',
+    "rest_framework_simplejwt",
+    "drf_yasg",
 
     "users",
     "materials",
