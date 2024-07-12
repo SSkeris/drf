bcf7e0985cbcc1550e1483533bc65e7c06a3b2b8
Stas <stasfuryus@gmail.com>
2024-07-12 13:03:15 +0300

    add CELERY_BEAT_SCHEDULE

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,13 @@ STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
+CELERY_BEAT_SCHEDULE = {
+    'check_last_login': {
+        'task': 'users.tasks.check_last_login',
+        'schedule': timedelta(minutes=10),
+    },
+}
+
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
