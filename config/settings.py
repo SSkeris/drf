5bcd3c214130ac784a48973e1922219d16c800f5
Stas <stasfuryus@gmail.com>
2024-07-12 13:03:32 +0300

    edit CELERY_BEAT_SCHEDULE

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 CELERY_BEAT_SCHEDULE = {
     'check_last_login': {
         'task': 'users.tasks.check_last_login',
-        'schedule': timedelta(minutes=10),
+        'schedule': timedelta(seconds=10),
     },
 }
 
